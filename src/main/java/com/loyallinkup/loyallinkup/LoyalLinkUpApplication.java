457d6afc6ce8a2765445f720a1a175c6a8d811bb
Tamara Simikj <tamara.simic12@gmail.com>
2023-05-04 11:18:29 +0200

    Initial commit

---
+++ src/main/java/com/loyallinkup/loyallinkup/LoyalLinkUpApplication.java
@@ -0,0 +1,13 @@
+package com.loyallinkup.loyallinkup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyalLinkUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyalLinkUpApplication.class, args);
+    }
+
+}
